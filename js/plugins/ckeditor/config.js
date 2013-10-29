31ca596dda4ad9e0fb4d5607babc5b3f57e65ae7
Brice Shatzer <brice.shatzer@gmail.com>
2013-10-29 11:14:26 -0500

    pulled samples out of ckeditor

--- js/plugins/ckeditor/config.js
+++ js/plugins/ckeditor/config.js
@@ -24,11 +24,11 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'others' },
 		{ name: 'about' }
 	];
-
 	// The default plugins included in the basic setup define some buttons that
 	// we don't want too have in a basic editor. We remove them here.
 	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
-
 	// Let's have it basic on dialogs as well.
 	config.removeDialogTabs = 'link:advanced';
+	//enable spellcheck
+	config.disableNativeSpellChecker = false;
 };
